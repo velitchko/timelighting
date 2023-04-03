0b5deb5937d4320a90f8199bcb607dc5438e44fd
velitchko <velitchko.filipov@tuwien.ac.at>
2023-04-03 12:57:59 +0200

    implement graph service to load files and parse data

--- temporal-graphs/src/app/services/graph.service.ts
+++ temporal-graphs/src/app/services/graph.service.ts
@@ -1,15 +1,73 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import Graph from '../types/graph.type';
+import Node from '../types/node.type';
+import { DATASETS } from './datasets';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GraphService {
 
-  constructor() { }
+  private graph: Graph;
 
+  constructor() { 
+    this.graph = {
+      nodes: [],
+      edges: []
+    };
+  }
+
+  // load data from json file
+  public loadData(dataset?: string): void {
+    const data = DATASETS[dataset ? dataset : 'vandebunte'];
+    this.parseData(data);
+  }
+
+  // parse data from json file
+  private parseData(data: any): void {
+    // iterate over nodes
+    data.graphnodes.forEach((node: any) => {
+      // iterate over each nodes position array
+      const id = node.id;
+
+      node.position.forEach((position: any) => {
+
+        // remove brackets and split on comma
+        const parsedPosition = position.string.replace(/[\])}[{(]/g, '');
+
+        const parts = parsedPosition.split(':');
+        
+        const times = parts[0].split(',');
+        const coords = parts[1].split(',');
+
+        // check if node already exists
+        const found = this.graph.nodes.find((node: Node) => node.id === id);
+
+        // if node exists, add coordinates and time
+        if (found) {
+          found.coordinates.push({ x: coords[0], y: coords[1] });
+          found.coordinates.push({ x: coords[2], y: coords[3] });
+
+          found.time.push(times[0]);
+          found.time.push(times[1]);
+        } 
+        // else create new node
+        else {
+          const newNode: Node = {
+            id: id,
+            label: `node-${node.id}`,
+            time: [times[0], times[1]],
+            coordinates: [{ x: coords[0], y: coords[1] }, { x: coords[2], y: coords[3] }]
+          };
+          this.graph.nodes.push(newNode);
+        }
+      });
+    });
+  }
+
+  // return graph as observable
   public getGraph(): Observable<Graph> {
-    return of({} as Graph);
+    return of(this.graph);
   }
 }
