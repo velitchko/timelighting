6d647db5b8de6bf263745ce7a64bfe6d83e87389
Alessio Arleo <alessio.arleo@tuwien.ac.at>
2023-04-12 11:22:06 +0200

    Added dataset files plus updated datasets.ts

--- temporal-graphs/src/app/services/datasets.ts
+++ temporal-graphs/src/app/services/datasets.ts
@@ -1,6 +1,14 @@
 import * as vandebunte from '../../assets/vandebunt.json';
+import * as rugby from '../../assets/rugby.json';
+import * as newcomb from '../../assets/newcomb.json';
+import * as pride from '../../assets/pride.json';
+import * as infovis from '../../assets/infovis.json';
 
 
 export const DATASETS: any = {
-    vandebunte: vandebunte
+    vandebunte: vandebunte,
+    rugby: rugby,
+    newcomb: newcomb,
+    dialogs: pride,
+    infovis: infovis
 };
